3531fc3613c211a055c957b3c91e9d9f6c80e60b
sergi galceran <sergi_galce7@hotmail.com>
2017-06-16 16:41:15 +0200

    arreglo propietario mapa

--- www/views/results/results.js
+++ www/views/results/results.js
@@ -10,11 +10,20 @@ angular.module('myApp.results', ['ngMaterial', 'ui.router'])
 
         $scope.books=$rootScope.libros;
         $rootScope.libros={};
+        
+        $http.get(API + '/users')
+            .then(function (response) {
+                $scope.users = response.data;
+                //$scope.getBooks();
+            }, function (error) {
+                console.log('Error al obtener los usuarios: ' + error.data);
+            });
+
 
         $scope.searchBook = function (book) {
         $rootScope.booksel=book;
         console.log("libro sel" + $rootScope.booksel)
         $state.go("bookDetail")
-    }
+        }
 
     })
